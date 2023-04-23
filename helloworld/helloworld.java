50d28059757b500dad7fe253524899b339a1e568
applepielinux <cgarcia1759@students.mc3.edu>
2023-04-22 21:22:09 -0400

    create hello world
    Simple java program to print hello world to user

---
+++ helloworld/helloworld.java
@@ -0,0 +1,13 @@
+/** Simple Java program. 
+@Author C. Garcia
+Program will print "hello world." to user.
+
+*/
+
+public class helloworld{
+     
+     public static void main(String[] args){
+      
+         System.out.println("hello world.");
+     }
+}
